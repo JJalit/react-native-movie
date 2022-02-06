1eed9b5bfa9a109439bf8dc5cca7e97afcb74797
jaeha <jaeha23@naver.com>
2022-02-06 20:05:43 +0900

    create context

--- src/Context/User/index.tsx
+++ src/Context/User/index.tsx
@@ -1,12 +1,55 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const User = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+const defaultContext: IUserContext = {
+  isLoading: false,
+  userInfo: undefined,
+  login: (email: string, password: string) => {},
+  getUserInfo: () => {},
+  logout: () => {},
 };
 
-export default User;
+const UserContext = createContext(defaultContext);
+
+interface Props {
+  children: JSX.Element | Array<JSX.Element>;
+}
+
+const UserContextProvider = ({ children }: Props) => {
+  const [userInfo, setUserInfo] = useState<IUserInfo | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const login = (email: string, password: string): void => {
+    AsyncStorage.setItem('token', 'ab1234').then(() => {
+      setUserInfo({ name: 'jaeha', email: 'jaeha23@naver.com' });
+      setIsLoading(true);
+    });
+  };
+
+  const getUserInfo = (): void => {
+    AsyncStorage.getItem('token')
+      .then(value => {
+        if (value) {
+          setUserInfo({ name: 'jaeha', email: 'jaeha23@naver.com' });
+          setIsLoading(true);
+        }
+      })
+      .catch(() => {
+        setUserInfo(undefined);
+        setIsLoading(true);
+      });
+  };
+
+  const logout = (): void => {
+    AsyncStorage.removeItem('token');
+    setUserInfo(undefined);
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  return <UserContext.Provider value={{ isLoading, userInfo, login, getUserInfo, logout }}>{children}</UserContext.Provider>;
+};
+
+export { UserContext, UserContextProvider };
