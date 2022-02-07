f01258099027e7372c2defacfce1cba28aa61a72
jaeha <jaeha23@naver.com>
2022-02-07 12:06:27 +0900

    create MovieDetail

--- src/Screens/MovieDetail/CastList/index.tsx
+++ src/Screens/MovieDetail/CastList/index.tsx
@@ -1,11 +1,51 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { FlatList } from 'react-native';
+import styled from 'styled-components/native';
 
-const CastList = () => {
+const Container = styled.View``;
+const Title = styled.Text`
+  font-size: 16px;
+  color: #ffffff;
+  font-weight: bold;
+  padding: 24px 16px 8px 16px;
+`;
+const CastContainer = styled.View`
+  padding: 0px 4px;
+`;
+const CastImage = styled.Image``;
+const LabelName = styled.Text`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 4px 2px;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+interface Props {
+  cast: Array<ICast>;
+}
+
+const CastList = ({ cast }: Props) => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <Container>
+      <Title>배우</Title>
+      <FlatList
+        horizontal={true}
+        data={cast}
+        keyExtractor={(item, index) => {
+          return `castList-${index}`;
+        }}
+        renderItem={({ item, index }) => (
+          <CastContainer>
+            <CastImage source={{ uri: (item as ICast).url_small_image }} style={{ width: 100, height: 150 }} />
+            <LabelName>{(item as ICast).name}</LabelName>
+          </CastContainer>
+        )}
+      />
+    </Container>
   );
 };
 
