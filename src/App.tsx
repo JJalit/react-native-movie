131158239422d2a2e0b636ec75aa1499e00dd6b6
jaeha <jaeha23@naver.com>
2022-02-07 10:00:35 +0900

    create App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
-import { StatusBar } from 'react-native';
+import { LogBox, StatusBar } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 import { UserContextProvider } from './Context/User';
 import Navigator from './Screens/Navigator';
+
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
 
 const App = () => {
   useEffect(() => {
